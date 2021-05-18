4ed4ed71b440580b22caa7346962bbc2d0838502
Andrew Joshua Cheyney Wilson <ajcheyneywilson@gmail.com>
2021-05-17 20:21:55 -0400

    added more

--- src/main.js
+++ src/main.js
@@ -4,7 +4,31 @@ document.addEventListener('readystatechange', e => {
     }
 });
 function init() {
-    document.querySelectorAll(".navbar.drop-down").forEach(elm => {
-        elm.firstChild;
+    let header = document.querySelector(".header");
+    let nav_btn = document.querySelector(".drop-down-btn");
+    let nav_btn_inner = document.querySelector(".btn-inner");
+    nav_btn.addEventListener("click", e => {
+        ;
+        let collapse_anim = header.animate([
+            { top: "-100%" },
+            { top: "0%" },
+        ], {
+            duration: 200,
+            easing: "ease-in"
+        });
+        let btn_anim = nav_btn_inner.animate([
+            { rotate: "0deg" },
+            { rotate: "135deg" },
+        ], 300);
+        if (header.classList.contains("collapsed")) {
+            header.classList.remove("collapsed");
+            collapse_anim.play();
+            btn_anim.play();
+        }
+        else {
+            collapse_anim.reverse();
+            header.classList.add("collapsed");
+            btn_anim.reverse();
+        }
     });
 }
--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,34 @@ document.addEventListener('readystatechange', e => {
 
 
 function init() {
-	document.querySelectorAll(".navbar.drop-down").forEach(elm => {
-		elm.firstChild
-	})
+	let header = document.querySelector<HTMLDivElement>(".header");
+	let nav_btn = document.querySelector<HTMLButtonElement>(".drop-down-btn");
+	let nav_btn_inner = document.querySelector<HTMLDivElement>(".btn-inner");
+
+	nav_btn.addEventListener("click", e=> {;
+		let collapse_anim = header.animate([
+			{ top: "-100%" },
+			{ top: "0%" },
+		], {
+			duration: 200,
+			easing: "ease-in"
+		} );
+
+		let btn_anim = nav_btn_inner.animate([
+			{ rotate: "0deg" },
+			{ rotate: "135deg" },
+		], 300 );
+
+		if (header.classList.contains("collapsed")) { 
+			header.classList.remove("collapsed");
+			collapse_anim.play();
+			btn_anim.play();
+		} else {
+			collapse_anim.reverse();
+			header.classList.add("collapsed");
+			btn_anim.reverse();
+		}
+	});
+
+
 }
