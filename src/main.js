4e363173501cefedc02b8a7ebed921dfa61685cd
Andrew Wilson <ajcheyneywilson@gmail.com>
2022-09-14 13:10:41 -0400

    shows portfolio first

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 const cursors = ["alias","all-scroll","auto","cell","context-menu","col-resize","copy","crosshair","default","e-resize","ew-resize","grab","grabbing","help","move","n-resize","ne-resize","nesw-resize","ns-resize","nw-resize","nwse-resize","no-drop","none","not-allowed","pointer","progress","row-resize","s-resize","se-resize","sw-resize","text","vertical-text","w-resize","wait","zoom-in","zoom-o"];
 
 document.addEventListener('readystatechange', ()=>{
+
 	switch (document.readyState) {
 		case 'interactive': {
 			// console.clear();
@@ -40,7 +41,6 @@ let n2 = new SimplexNoise(1);
 let aspect;
 let canvas;
 let c;
-
 
 function init_document() {
 
